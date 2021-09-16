80db286b4daa5d1d00e4cb17ca1ecc839b175783
Paul Grudzien <63060033+PaulGrudzien@users.noreply.github.com>
2021-09-16 22:31:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,26 +3,33 @@ const bouton = document.querySelector('input')
 const par = document.querySelector('p.consigne')
 const histo = document.querySelector('div.historique')
 const faces = ['zzz.png', 'etoile.png', 'bouclier.png']
+const bgs = ['blue', 'black', 'white']
 const des = [{face:'zzz.png', relancer:true}, {face:'zzz.png', relancer:true}, {face:'zzz.png', relancer:true}, {face:'zzz.png', relancer:true}, {face:'zzz.png', relancer:true}]
 
 function shake(element) {
-  element.className = "";
+  element.className = "de";
   window.requestAnimationFrame(function(time) {
     window.requestAnimationFrame(function(time) {
-      element.className = "shake";
+      element.className = "de shake";
     });
   });
 }
 
 function lancer() {
     des.forEach((de, i) => {
+        const div = document.createElement("div")
         const image = document.createElement("img")
-        de.face = faces[Math.floor(Math.random()*faces.length)]
+        const a = Math.floor(Math.random()*faces.length)
+        de.face = faces[a]
+        de.bg = bgs[a]
         de.relancer = true
         image.src = de.face
-        image.id = i
-        tapis.appendChild(image)
-        shake(image)
+        div.style.backgroundColor = de.bg
+        div.id = i
+        div.className = "de"
+        div.appendChild(image)
+        tapis.appendChild(div)
+        shake(div)
     })
     bouton.value = "Relancer"
 	bouton.className = "orange"
@@ -32,10 +39,10 @@ function lancer() {
 function selectionner(i) {
     const image = document.getElementById(i)
     if (des[i].relancer) {
-        image.className = "garder"
+        image.className = "de garder"
         des[i].relancer = false
     } else {
-        image.className = ""
+        image.className = "de"
         des[i].relancer = true
     }
 }
@@ -43,10 +50,14 @@ function selectionner(i) {
 function relancer() {
     des.forEach((de, i) => {
         if (de.relancer) {
-            const image = document.getElementById(i)
-            de.face = faces[Math.floor(Math.random()*faces.length)]
+            const div = document.getElementById(i)
+            const image = div.firstChild
+            const a = Math.floor(Math.random()*faces.length)
+            de.face = faces[a]
+            de.bg = bgs[a]
             image.src = de.face
-            shake(image)
+            div.style.backgroundColor = de.bg
+            shake(div)
         }
     })
     bouton.value = "Ramasser"
@@ -58,18 +69,22 @@ function ramasser() {
     tapis.innerHTML = ''
     bouton.value = "Lancer"
 	bouton.className = "vert"
-	const div = document.createElement("div")
+	const mainDiv = document.createElement("div")
 	des.forEach(de => {
+	    const div = document.createElement("div")
 	    const image = document.createElement("img")
 	    image.src = de.face
+        div.style.backgroundColor = de.bg
+        div.className = "de"
 	    div.appendChild(image)
+	    mainDiv.appendChild(div)
 	})
-	histo.insertBefore(div, histo.firstChild);
+	histo.insertBefore(mainDiv, histo.firstChild);
 }
 
 function handleClick(event) {
     if (event.target.tagName == 'IMG' && bouton.value == 'Relancer') {
-        selectionner(event.target.id)
+        selectionner(event.target.parentNode.id)
     } else if (event.target.tagName == 'INPUT') {
         if (event.target.value == 'Lancer') {lancer()}
         else if (event.target.value == 'Relancer') {relancer()}
